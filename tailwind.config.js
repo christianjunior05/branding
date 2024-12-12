aff98c8fca53ebdc5e40d99588d2eaec55eaa15e
christianjunior05 <bchrist.junior@gmail.com>
2024-12-12 08:10:43 +0000

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["index.html" ,"./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+          "blue":"#219ebc",
+      }
+    },
   },
   plugins: [],
 }
